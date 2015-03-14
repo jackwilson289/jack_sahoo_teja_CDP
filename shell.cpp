98903e3ae34d59ebbd820544f597ebe46699de13
Hrudaya Ranjan Sahoo <hrudayranjansahoo0208@gmail.com>
2015-03-14 19:23:01 +0530

    Lists the content of the directory

--- shell.cpp
+++ shell.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+#include <sys/types.h>
+#include <dirent.h>
 using namespace std;
 
 int main ()
 {
 	bool cont = true;
 	string command;
+	string dir_name;
 
 	while (cont)
 	{
@@ -16,6 +19,25 @@ int main ()
 		{
 			cont = false;
 		}
+
+		else if (command == "dir")
+		{
+			cout << "Enter the directory name: ";
+			cin  >> dir_name;
+
+			DIR* dirp = opendir(dir_name.c_str());
+			struct dirent* dp;
+
+			if (dirp) 
+			{
+			    while ((dp = readdir(dirp)) != NULL) 
+			    {
+			        cout << (dp -> d_name) << endl;
+			    } 
+			}
+		}
+
+
 	}
 
 	return 0;
