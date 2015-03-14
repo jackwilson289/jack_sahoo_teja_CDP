c72ee8b2471af088668db182b502a184cb485360
Hrudaya Ranjan Sahoo <hrudayranjansahoo0208@gmail.com>
2015-03-14 18:53:21 +0530

    c++ file for shell

--- shell.cpp
+++ shell.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main ()
 {
-    cout << "Hello World!\n";
-    return 0;
+	bool cont = true;
+	string command;
+
+	while (cont)
+	{
+		cout << ">>>  ";
+		cin >> command;
+
+		if (command == "quit")
+		{
+			cont = false;
+		}
+	}
+
+	return 0;
 }
